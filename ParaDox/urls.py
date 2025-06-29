fb1d5674eeb6aa443ebc9ac828132261c7bdb32a
arthp <agrearth22@gmail.com>
2025-06-29 17:43:27 +0530

    pdf viewer problem solved

--- ParaDox/urls.py
+++ ParaDox/urls.py
@@ -51,9 +51,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.dashboard.urls'))
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
- 
+    path('', include('apps.dashboard.urls')),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
